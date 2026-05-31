30950076c85b737798a557dfdb32915da4d8f8c9
JoGuevara28 <106563305+JoGuevara28@users.noreply.github.com>
2026-05-30 20:47:28 -0600

    Update player.py

--- player.py
+++ player.py
@@ -1,6 +1,27 @@
+"""
+Created by Jorge Guevara Chavarría
+
+Player class for Shadow Dungeon RPG.
+
+This class represents the player.
+It stores player attributes such as health, attack power,
+defense, gold, and potions. It also contains methods for
+taking damage, healing, checking if the player is alive,
+and displaying player statistics.
+"""
+
 class Player:
 
-    def __init__(self, name, health = 100, max_health = 100, attack_power = 15, defense = 5, gold = 0, potions = 2):
+    def __init__(
+        self,
+        name,
+        health=100,
+        max_health=100,
+        attack_power=15,
+        defense=5,
+        gold=0,
+        potions=2
+    ):
         self.name = name
         self.health = health
         self.max_health = max_health
@@ -10,24 +31,36 @@ class Player:
         self.potions = potions
 
     def show_stats(self):
-        print(self.name)
-        print(self.health)
-        print(self.max_health)
-        print(self.attack_power)
-        print(self.defense)
-        print(self.gold)
-        print(self.potions)
+        """Displays the player's current statistics."""
+        print("=============Player Stats=============")
+        print(f"Name: {self.name}")
+        print(f"HP: {self.health}/{self.max_health}")
+        print(f"Attack: {self.attack_power}")
+        print(f"Defense: {self.defense}")
+        print(f"Gold: {self.gold}")
+        print(f"Potions: {self.potions}")
 
     def take_damage(self, amount):
+        """
+        Reduces the player's health based on incoming damage.
+
+        The player's defense reduces the final damage taken.
+        Health cannot go below 0.
+        """
         real_damage = amount - self.defense
         if real_damage < 0:
             real_damage = 0
-        self.health = self.health - real_damage
-        if self.health <= 0:
+        self.health -= real_damage
+        if self.health < 0:
             self.health = 0
         return real_damage
 
     def heal(self):
+        """
+        Uses one potion to heal the player.
+        The player heals 30 HP per potion.
+        Health cannot go above max_health.
+        """
         if self.potions <= 0:
             return "You don't have potions"
         self.health += 30
@@ -35,10 +68,10 @@ class Player:
         if self.health > self.max_health:
             self.health = self.max_health
         print(f"You have {self.health}/{self.max_health} HP")
-        return 'You used a potion'
-        
+        return "You used a potion"
 
     def is_alive(self):
+        """Returns True if the player is still alive."""
         if self.health <= 0:
             return False
         return True
