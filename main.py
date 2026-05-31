c11cc09a3d1dc63f65832e936586211051dd3c2f
JoGuevara28 <106563305+JoGuevara28@users.noreply.github.com>
2026-05-30 20:47:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+"""
+Created by Jorge Guevara Chavarría
+
+Shadow Dungeon RPG
+Main entry point of the program.
+
+This file only starts the game.
+The main game logic is handled by the Game class.
+"""
+
 from game import Game
 
 game = Game()
